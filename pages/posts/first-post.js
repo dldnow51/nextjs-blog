7b797729313d2143483e28ae9010ec40fb30e49f
dldnow51 <dldnow51@naver.com>
2023-04-07 19:55:43 +0900

    qwer

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,12 @@
+import Link from 'next/link';
+
 export default function FirstPost() {
-    return <h1>Lee Woojae</h1>;
-  }
+  return (
+    <>
+      <h1>Lee Woojae</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
